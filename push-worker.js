cb5555e06109d5f9e55dac445ebf32d787eef89c
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2021-09-01 13:20:07 -0700

    update with Airship

---
+++ push-worker.js
@@ -0,0 +1,13 @@
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js');
+uaSetup.worker(self, {
+	defaultIcon:
+		'https://images.unsplash.com/photo\u002D1630101129455\u002Db7b74fa5788d?ixid\u003DMnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8\u0026ixlib\u003Drb\u002D1.2.1\u0026auto\u003Dformat\u0026fit\u003Dcrop\u0026w\u003D1500\u0026q\u003D80',
+	defaultTitle: 'Sign in to access list',
+	defaultActionURL:
+		'https://a\u002Dplace\u002Dto\u002Dgo\u002Dd3310.web.app/signin',
+	appKey: 'mIPLg9r0QCKhhlP0rwu8CA',
+	token:
+		'MTptSVBMZzlyMFFDS2hobFAwcnd1OENBOkV4QkxRWmpVZnh2SUN0NG1nclgzWGkydE1jSDlfOW1CUWE4X0FwN2VacTA',
+	vapidPublicKey:
+		'BJ7w1Pkr-xRs9D8JZz4E-9TmJyF9BSzTJTEIup_-_BETQkhRFXbJo2PdDXyHqlMCO2EuI04-TGAWZIv9mmMm3m4=',
+});
